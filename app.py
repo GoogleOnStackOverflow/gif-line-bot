bd1f81a95586be70c4b8b3b6efbbdce0c848be6f
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2017-10-27 11:12:17 +0800

    debug

--- app.py
+++ app.py
@@ -69,12 +69,12 @@ def callback():
                 print event.message
                 
                 result = requests.get(tenor_json_api + '&q=' + event.message.text + '&limit=5')
-                print result.json()
+                print result.json()['results'][0]['media']['gif']
                 #return_message = result.json().results[0].media.gif.url
                 #print return_message
                 line_bot_api.push_message(
                     event.source.sender_id,
-                    TextSendMessage(text=result.text)
+                    TextSendMessage(text='Hello')
                 )
                 """
                 if(feature_enabled):
